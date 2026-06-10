19046dbce8e0013c504c5f946014469c2a0825c3
Mrferny123 <nissiann@icloud.com>
2026-06-09 20:14:30 -0400

    Initial Commit

---
+++ Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by Nissi Sanju on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RippleTests/RippleTests.swift
@@ -0,0 +1,19 @@
+//
+//  RippleTests.swift
+//  RippleTests
+//
+//  Created by Nissi Sanju on 6/9/26.
+//
+
+import Testing
+@testable import Ripple
+
+struct RippleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
