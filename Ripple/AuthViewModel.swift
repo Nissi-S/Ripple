3806ee518fcc1c5bfc7a0ec4ba909ee43ff7ca84
Mrferny123 <nissiann@icloud.com>
2026-06-13 20:41:17 -0400

    started making supabase backend with sign out, sign in, and sign up - followed tutorial

---
+++ Ripple/AuthViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  AuthViewModel.swift
+//  Ripple
+//
+//  Created by Nissi Sanju on 6/13/26.
+//
+
+import SwiftUI
+import Supabase
+import Combine
+
+@MainActor
+
+class AuthViewModel: ObservableObject {
+    @Published var session: Session?
+    @Published var isAuthenticated = false
+    
+    func getIntialSession() async {
+        do {
+            let current = try await supabase.auth.session
+            self.session = current
+            self.isAuthenticated = current != nil
+        } catch {
+            print("No active session: \(error.localizedDescription)")
+        }
+    }
+    
+    func signUp(email: String, password: String) async {
+        do {
+            let result = try await supabase.auth.signUp(email: email, password: password)
+            self.session = result.session
+            self.isAuthenticated = self.session != nil
+        }
+        catch {
+            print("Sign up failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func signIn(email: String, password: String) async {
+        do {
+            let result = try await supabase.auth.signIn(email: email, password: password)
+            self.session = result
+            self.isAuthenticated = self.session != nil
+        }
+        catch {
+            print("Sign In failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func signOut() async {
+        do {
+            try await supabase.auth.signOut()
+            self.session = nil
+            self.isAuthenticated = false
+        }
+        catch {
+            print("Sign Out failed: \(error.localizedDescription)")
+        }
+    }
+    
+}
---
+++ Ripple/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  Ripple
+//
+//  Created by Nissi Sanju on 6/13/26.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://gcaabpvaswkezgkvabci.supabase.co")!,
+  supabaseKey: "sb_publishable_T3dz9j-2Dyv_d2kagyQkUA_fvtDLW6z"
+)
